3731e9e87d857e569736077f4854a8771f7f7e5a
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-01 11:47:03 +0200

    fix

--- Scripts/Body.gd
+++ Scripts/Body.gd
@@ -1,5 +1,24 @@
 class_name Body
 extends CharacterBody2D
 
-@export var life: int
+@export var max_life = 3
+@onready var life = max_life : set = _set_life
 @export var speed: float
+
+signal health_updated(life)
+signal killed()
+
+func damage(amount):
+	_set_life(life-amount)
+
+func kill():
+	pass
+
+func _set_life(value):
+	var prev_life = life
+	life = clamp(value, 0, max_life)
+	if life != prev_life:
+		emit_signal("life updated", life)
+		if life == 0:
+			kill()
+			emit_signal("killed")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,17 +3,10 @@ extends Body
 
 enum EPlayer { Human, Ghost }
 
-signal health_updated(life)
-signal killed()
-
-@export var speed: float
 @export var player: EPlayer
 @export var bullet_speed: float
 
 var bullet = load("res://Scenes/bullet.tscn")
-
-@export var max_life = 3
-@onready var life = max_life : set = _set_life
  
 
 var player1: Player
@@ -36,10 +29,8 @@ func get_direction():
 	return direction
 
 func _ready():
-	if player == EPlayer.Human:
-		player1 = self
-	elif player == EPlayer.Ghost:
-		player2 = self
+	player1 = get_node("../Player1")
+	player2 = get_node("../Player2")
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack_1"):
@@ -50,25 +41,6 @@ func _process(delta):
 func _physics_process(delta):
 	velocity = get_direction() * speed
 	move_and_slide()
-	
-	
-
-func damage(amount):
-	_set_life(life-amount)
-
-func kill():
-	pass
-
-func _set_life(value):
-	var prev_life = life
-	life = clamp(value, 0, max_life)
-	if life != prev_life:
-		emit_signal("life updated", life)
-		if life == 0:
-			kill()
-			emit_signal("killed")
-	
-
 
 func _on_input_event(viewport, event, shape_idx):
 	pass
