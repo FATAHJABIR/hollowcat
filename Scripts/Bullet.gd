7adefa001bb0c8f907088c3af26c1884a3fe8c04
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-01 15:07:31 +0200

    fix

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -4,9 +4,6 @@ extends RigidBody2D
 var bullet_owner: Body
 
 func _on_body_entered(body):
-	if body == bullet_owner:
-		return
-	
-	if body is Body:
-		body.life -= owner.damage
-		queue_free()
+	print("kiiuh")
+	body.life -= owner.damage
+	queue_free()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,11 +8,12 @@ enum EPlayer { Human, Ghost }
 @export var bullet_speed: float
 
 var bullet = load("res://Scenes/bullet.tscn")
- 
+
 var rifu = max_life
 
+var last_normalized_direction: Vector2 = Vector2.RIGHT
+var direction: Vector2
 
- 
 
 var player1: Player
 var player2: Player
@@ -20,19 +21,19 @@ var player2: Player
 
 
 func get_direction():
-	var direction: Vector2
+	var new_direction: Vector2
 	
 
 	if player == EPlayer.Human:
-		direction.x = Input.get_axis("left_1", "right_1")
-		direction.y = Input.get_axis("up_1", "down_1")  
+		new_direction.x = Input.get_axis("left_1", "right_1")
+		new_direction.y = Input.get_axis("up_1", "down_1")  
 	elif player == EPlayer.Ghost:
-		direction.x = Input.get_axis("left_2", "right_2")
-		direction.y = Input.get_axis("up_2", "down_2")
+		new_direction.x = Input.get_axis("left_2", "right_2")
+		new_direction.y = Input.get_axis("up_2", "down_2")
 	else:
 		print("Player %s does not exist" % player)
 	
-	return direction
+	return new_direction
 
 func _ready():
 	player1 = get_node("../Player1")
@@ -40,22 +41,24 @@ func _ready():
 	
 
 func _process(delta):
-	if Input.is_action_just_pressed("attack_1"):  
+	direction = get_direction()
+	if direction.length_squared() > 0.0001:
+		last_normalized_direction = direction.normalized()
+	
+	if Input.is_action_just_pressed("attack_1") and player == EPlayer.Human \
+	or Input.is_action_just_pressed("attack_2") and player == EPlayer.Ghost:  
 		var new_bullet: Bullet = bullet.instantiate()
-		new_bullet.linear_velocity = get_direction().normalized() * bullet_speed
+		new_bullet.apply_central_impulse(last_normalized_direction * bullet_speed)
+		new_bullet.position = position
+		get_tree().root.add_child(new_bullet)
 		
 		
 func _physics_process(delta):
-	velocity = get_direction() * speed
+	velocity = direction * speed
 	move_and_slide()
 
-	
-	
-
-
-	
-
 func _on_monster_detector_body_entered(body):
+	print("onaojnawd")
 	if body is Monster:
 		print("help")
 		$AnimationPlayer.play("Red")
