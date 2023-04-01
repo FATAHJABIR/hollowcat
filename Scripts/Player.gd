583c1bc2400af33e523395a2b66c48d2af7d2ec8
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-01 07:30:35 +0200

    small changes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
 
-enum EPlayer { Player1, Player2 }
+enum EPlayer { Human, Ghost }
 
 @export var speed: float
 @export var player: EPlayer
+@export var life: int
 
 func get_direction():
 	var direction: Vector2
 	
-	if player == EPlayer.Player1:
+	if player == EPlayer.Human:
 		direction.x = Input.get_axis("left_1", "right_1")
 		direction.y = Input.get_axis("up_1", "down_1")
-	elif player == EPlayer.Player2:
+	elif player == EPlayer.Ghost:
 		direction.x = Input.get_axis("left_2", "right_2")
 		direction.y = Input.get_axis("up_2", "down_2")
 	else:
