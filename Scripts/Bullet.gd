f1a52e176b6bf330348531ac1bff19a42b3638df
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-01 15:32:08 +0200

    fix layers

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,7 +1,9 @@
 class_name Bullet
 extends RigidBody2D
 
+var bullet_owner: Body
+
 func _on_body_entered(body):
 	print("kiiuh")
-	body.life -= owner.damage
+	body.life -= bullet_owner.damage
 	queue_free()
