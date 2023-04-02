205cf73cd9c17e5b286638f64a8182699a9645e5
Conti Aymeric <aymeric.conti@student-cs.fr>
2023-04-02 17:53:44 +0200

    adding text

---
+++ Scenes/LabelSwitch.gd
@@ -0,0 +1,14 @@
+extends Panel
+
+var current_label: int = 0
+@onready var labels: Array = [get_child(0), get_child(1), get_child(2)]
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		labels[current_label].visible = false
+		current_label += 1
+		if current_label <= 2:
+			labels[current_label].visible = true
+		else:
+			queue_free()
+		
